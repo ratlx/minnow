d33e7d12aa7822cad59532a66823ee9e8519ccbf
ratlx <louxu@hnu.edu.cn>
2025-07-25 11:40:55 +0800

    lab 0 3.5 Writing webget

--- apps/webget.cc
+++ apps/webget.cc
@@ -10,7 +10,25 @@ using namespace std;
 void get_URL( const string& host, const string& path )
 {
   cerr << "Function called: get_URL(" << host << ", " << path << ")\n";
-  cerr << "Warning: get_URL() has not been implemented yet.\n";
+  // cerr << "Warning: get_URL() has not been implemented yet.\n";
+  Address address { host, "http" };
+  TCPSocket socket;
+  socket.connect( address );
+  string request = "GET " + path + " HTTP/1.1\r\n"
+                   "Host: " + host + "\r\n"
+                   "Connection: close\r\n"
+                   "\r\n";
+  socket.write( request );
+  string response;
+  while ( true ) {
+    string buffer;
+    socket.read( buffer );
+    if ( buffer.empty() ) {
+      break; 
+    }
+    response += buffer;
+  }
+  cout << response;
 }
 
 int main( int argc, char* argv[] )
