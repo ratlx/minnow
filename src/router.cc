eb641c89c7bbe91b5b5b450b40598a63545eac75
ratlx <louxu@hnu.edu.cn>
2025-08-18 21:49:18 +0800

    check6: resume  cerr

--- src/router.cc
+++ src/router.cc
@@ -16,6 +16,10 @@ void Router::add_route( const uint32_t route_prefix,
                         const optional<Address> next_hop,
                         const size_t interface_num )
 {
+  cerr << "DEBUG: adding route " << Address::from_ipv4_numeric( route_prefix ).ip() << "/"
+       << static_cast<int>( prefix_length ) << " => " << ( next_hop.has_value() ? next_hop->ip() : "(direct)" )
+       << " on interface " << interface_num << "\n";
+       
   if ( prefix_length > 32 ) {
     throw invalid_argument( "prefix_length should be LE than 32" );
   }
